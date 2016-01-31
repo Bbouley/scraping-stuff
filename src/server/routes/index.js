5ab0d6578b2a440dbc431a7b21c74729e521c3d2
bbouley <b.bouley@hotmail.co.uk>
2016-01-30 17:45:20 -0700

    grabbing the page values on meeting search

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,21 +1,37 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-var cheerio = require('cheerio')
+var cheerio = require('cheerio');
+var webdriver = require('selenium-webdriver'),
+    By = require('selenium-webdriver').By,
+    until = require('selenium-webdriver').until;
+
+var driver = new webdriver.Builder()
+    .forBrowser('firefox')
+    .build();
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.get('/meetinginfo', function(req, res, next) {
-    url = 'http://www.daccaa.org/query.asp';
+    driver.get('http://www.daccaa.org/meetings.htm');
+    // driver.click('name="cmdFindMeetings"');
+    var searchMeetings = driver.findElement(webdriver.By.name('cmdFindMeetings'));
+    console.log(searchMeetings.click());
+    driver.quit();
 
-    request(url, function(err, res, html) {
-        if(!err) {
-            var pageData = cheerio.load(html);
-            res.send(pageData);
-        }
-    })
+
+
+    // url = 'http://www.daccaa.org/query.asp';
+
+    // var pageData = request(url, function(err, res, html) {
+    //     if(!err) {
+    //         var page = cheerio.load(html);
+    //         console.log(page.html())
+    //         return page ;
+    //     }
+    // });
 })
 
 module.exports = router;
