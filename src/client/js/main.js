85d481d5e0ae81e8dcd4d9dbcbce117c3e66c1b3
bbouley <b.bouley@hotmail.co.uk>
2016-01-30 21:00:35 -0700

    google maps issue with geocode

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -2,16 +2,22 @@
 
 $(document).on('ready', function() {
 
+    // var onlyPlaces;
+
+    var myMap;
+    function initMap() {
+      myMap = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 39.7392, lng: -104.9903},
+        zoom: 10
+      });
+    }
+    initMap();
+
     $('.getAll').on('click', function(e) {
         e.preventDefault();
 
         $.get('/meetinginfo', function(data) {
-            // console.log(data);
             var newArray = data.splice(14)
-            console.log(newArray);
-            // var addressLocation = newArray.filter(function(el, ind) {
-            //     return (ind % 3 === 0 ) || (ind % 4 === 0)
-            // })
             var chunk_size = 7;
             var groups = newArray.map( function(e,i){
                 return i%chunk_size===0 ? newArray.slice(i,i+chunk_size) : null;
@@ -19,9 +25,53 @@ $(document).on('ready', function() {
             .filter(function(e){
                 return e;
             });
-              console.log(groups)
+
+            // console.log(groups);
+
+            var timeAddressObjects = groups.map(function(el, index) {
+                return {
+                    name : el[2],
+                    day : el[0],
+                    time : el[1],
+                    street : el[3].split(' ').slice(0,3).join(' '),
+                    area : el[4]
+                };
+            });
+
+            timeAddressObjects.splice(1134, 7);
+            // console.log(timeAddressObjects[0])
+            var object1 = timeAddressObjects[0]
+
+            var geocoder = new google.maps.Geocoder();
+
+            console.log(makeAddress(object1));
+
+            function addLocation(geocoder, map, addressObject) {
+                geocoder.geocode({'address' : makeAddress(addressObject)}), function(results, status) {
+                    console.log('testing1');
+                    console.log(results, status);
+                    if(status === google.maps.GeocoderStatus.OK) {
+                        console.log('testing2');
+                        map.setCenter(results[0].geometry.location);
+                         var marker = new google.maps.Marker({
+                            map : map,
+                            position : results[0].geometry.location,
+                            title : addressObject.name + ' ' + addressObject.time
+                        });
+                    } else {
+                        console.log('Geocode Unsuccessful: ' + status);
+                    }
+                }
+            }
+
+            addLocation(geocoder, myMap, object1)
         });
 
     });
 
+function makeAddress (object) {
+    return (object.street + ', ' + object.area + ', CO').toString();
+}
+
 });
+
